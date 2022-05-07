17b35e0f7930838270d4d10c97fbff5d9cf15ecf
yangjianwei <2017585616@qq.com>
2022-05-07 17:54:02 +0800

    first commit

---
+++ src/main/java/com/deepblue/inaction_01_core_principles_and_case_analysis_of_large_scale_website_technology_architecture/chapter_03/controller/RequestController.java
@@ -0,0 +1,17 @@
+package com.deepblue.inaction_01_core_principles_and_case_analysis_of_large_scale_website_technology_architecture.chapter_03.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ */
+@Controller
+public class RequestController {
+
+    public static void request(HttpServletRequest request) {
+
+    }
+}
---
+++ src/main/java/com/deepblue/inaction_01_core_principles_and_case_analysis_of_large_scale_website_technology_architecture/chapter_03/model/RequestObject.java
@@ -0,0 +1,14 @@
+package com.deepblue.inaction_01_core_principles_and_case_analysis_of_large_scale_website_technology_architecture.chapter_03.model;
+
+/**
+ *
+ */
+public class RequestObject {
+
+    git init
+    git add .
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin git@github.com:yangyang0001/java_architecture.git
+    git push -u origin main
+}
