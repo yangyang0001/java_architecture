8a95655d0412b6fe8737e14429c90fd86fe3bbc3
yangjianwei <2017585616@qq.com>
2022-07-12 15:25:23 +0800

    微服务架构设计模式只FTGO

---
+++ src/main/java/com/deepblue/inaction_02_microservice_architecture_design_patterns/chapter_00/TestObject.java
@@ -0,0 +1,19 @@
+package com.deepblue.inaction_02_microservice_architecture_design_patterns.chapter_00;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ */
+public class TestObject {
+
+    public static void main(String[] args) {
+
+        Map<String, String> mineMap = new HashMap<>();
+
+        mineMap.containsKey("");
+
+
+    }
+}
